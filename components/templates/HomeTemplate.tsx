0fac9e80a64dd7b4285c00664bbb546f89039cbd
Felipe Freitas <felipe-amorim.freitas@bahag.com>
2023-08-08 18:44:29 +0200

    Layout header redesign, Sheet artists detail design and consume API data

--- components/templates/HomeTemplate.tsx
+++ components/templates/HomeTemplate.tsx
@@ -25,56 +25,81 @@ import {
   SheetTrigger,
 } from '../ui/sheet';
 import { Separator } from '../ui/separator';
-import React, { useEffect } from 'react';
 import { Badge } from '../ui/badge';
 import ChevronRight from '../ui/ChevronRight';
-import { Artist } from '@/app/page';
+import React from 'react';
+import Note from '../ui/Note';
 
 type HomeTemplateProps = {
   artists: Artist[];
-  instruments: string[];
 };
 
-const HomeTemplate = ({artists, instruments}: HomeTemplateProps) => {
-  const [slider, setSlider] = React.useState([1900, 2000]);
-  const [selectedInstruments, setSelectedInstruments] = React.useState(
-    instruments
-  );
+interface Artist {
+  "First Name": string;
+  "Surname": string;
+  "Instrument": string[];
+  "Picture Link": string;
+  "Date of Birth": string;
+  "Date of Death": string;
+  "Publications / Albums": string;
+  "Bands / Formations": string;
+  "Description": string;
+  "Sources": string;
+  "First Appearance": number;
+  "Last Appearance": number;
+}
+
+const HomeTemplate = ({ artists }: HomeTemplateProps) => {
+  const instruments = Array.from(new Set(artists.map(artist => artist['Instrument']).filter(Boolean).flat()));
+
+  const [slider, setSlider] = React.useState([1900, new Date().getFullYear()]);
+  const [selectedInstruments, setSelectedInstruments] = React.useState(instruments);
 
   const filteredArtists = artists.filter((artist: Artist) => {
     return (
-      artist.activeYears.from >= slider[0] &&
-      artist.activeYears.to <= slider[1] &&
-      artist.instrument.some((instrument: string) =>
+      artist['First Appearance'] >= slider[0] &&
+      artist['Last Appearance'] <= slider[1] &&
+      artist['Instrument'].some((instrument: string) =>
         selectedInstruments.includes(instrument)
       )
     );
   });
 
+  console.log(artists)
+
   return (
     <>
-      <div className='mb-10'>
-        <h1 className='text-4xl	font-bold mb-10 text-center'>Refocus Herstory</h1>
-        <p>
-          Welcome to "Hidden Jazz Gems: Unveiling Underrated Divas!" Discover
-          the mind-blowing talents of underappreciated female jazz artists
-          through curated profiles, rare recordings, and mind-boggling live
-          performances. Join our rad community and embark on a wicked musical
-          journey that'll leave you craving more. Get ready to groove with the
-          sickest hidden treasures!
-        </p>
+      <div className='flex flex-col gap-2 mb-12 justify-center items-center'>
+        <div className='flex justify-center items-center'>
+          <div className='flex flex-col text-left items-center'>
+            <h1 className='text-4xl	font-bold text-[#F66048]'>RefocusHerstory</h1>
+            <h2 className='text-xs	'>INSTRUMENTALISTINNEN DES 20. JAHRHUNDERTS</h2>
+          </div>
+          <Note />
+        </div>
+        <div className='w-10/12'>
+          <h2 className='text-2xl font-semibold	'>About the project</h2>
+          <p>
+            Welcome to "Hidden Jazz Gems: Unveiling Underrated Divas!" Discover
+            the mind-blowing talents of underappreciated female jazz artists
+            through curated profiles, rare recordings, and mind-boggling live
+            performances. Join our rad community and embark on a wicked musical
+            journey that'll leave you craving more. Get ready to groove with the
+            sickest hidden treasures!
+          </p>
+        </div>
       </div>
-      <div className='flex flex-col gap-4 w-10/12 items-center mt-6'>
+      <div className='flex flex-col gap-6 w-10/12 items-center mt-6'>
         <Slider
           defaultValue={slider}
           onValueChange={(v) => setSlider(v)}
           min={1900}
-          max={2000}
+          max={new Date().getFullYear()}
           thumbOne={slider[0]}
           thumbTwo={slider[1]}
         />
         <div className='flex gap-2'>
-          {instruments.map((instrument) => (
+          {instruments.length > 0 && instruments.map((instrument) => (
             <Toggle defaultPressed={true} key={instrument} onClick={
               () => setSelectedInstruments(
                 (state) => state.includes(instrument)
@@ -89,25 +114,25 @@ const HomeTemplate = ({artists, instruments}: HomeTemplateProps) => {
       </div>
       <Separator className='my-10' />
       <div className='flex flex-row gap-3 w-full flex-wrap items-center justify-center'>
-        {filteredArtists.map(({activeYears, description, artistName, imageUrl, instrument}) => (
-          <Card key={artistName} className='p-4 max-w-[430px]'>
+        {filteredArtists.map((artist) => (
+          <Card key={`${artist?.['First Name']} ${artist?.['Surname']}`} className='p-4 max-w-[430px]'>
             <CardHeader className='flex flex-row justify-between'>
             <img
-              src={imageUrl}
+              src={artist['Picture Link']}
               alt=''
               className='rounded-full object-cover w-[100px] h-[100px]'
               width={100}
               height={100}
             />
               <div className='text-right flex flex-col justify-between'>
-                <CardTitle>{artistName}</CardTitle>
-                <CardDescription>{`Aktive Zeit: ${activeYears.from} -  ${activeYears.to}`}</CardDescription>
+                <CardTitle>{`${artist?.['First Name']} ${artist?.['Surname']}`}</CardTitle>
+                <CardDescription>{`Aktive Zeit: ${artist?.['First Appearance']} -  ${artist?.['Last Appearance']}`}</CardDescription>
               </div>
             </CardHeader>
             <CardContent className='flex items-center gap-5'>
               <div className='flex flex-col gap-4'>
                 <div className='flex flex-row gap-1'>
-                  {instrument.map((instrument) => (
+                  {artist?.['Instrument']?.map((instrument) => (
                     <Badge key={instrument} variant='outline'>{instrument}</Badge>
                   ))}
                 </div>
@@ -120,7 +145,7 @@ const HomeTemplate = ({artists, instruments}: HomeTemplateProps) => {
                   allow="autoplay; clipboard-write; encrypted-media; fullscreen;picture-in-picture"
                   loading="lazy"
                 />
-                <p className='text-ellipsis max-h-max[40px] line-clamp-3'>{description}</p>
+                <p className='text-ellipsis max-h-max[40px] line-clamp-3'>{artist?.['Description']}</p>
               </div>
             </CardContent>
             <CardFooter className='flex flex-row-reverse'>
@@ -128,18 +153,44 @@ const HomeTemplate = ({artists, instruments}: HomeTemplateProps) => {
                 <SheetTrigger asChild>
                   <Button variant='outline'>MEHR <ChevronRight /></Button>
                 </SheetTrigger>
-                <SheetContent side={'right'}>
-                  <SheetHeader>
-                    <SheetTitle>Edit profile</SheetTitle>
-                    <SheetDescription>
-                      Make changes to your profile here. Click save when you're
-                      done.
-                    </SheetDescription>
+                <SheetContent side={'right'} className="overflow-y-auto sm:max-w-[50%]">
+                  <SheetHeader className='mb-4'>
+                    <img
+                      src={artist?.['Picture Link']}
+                      alt=''
+                      className='object-cover max-h-[50vh]'
+                      width={'100%'}
+                    />
+                    <div className='px-6'>
+                      <SheetTitle className=''>{`${artist?.['First Name']} ${artist?.['Surname']}`}</SheetTitle>
+                      <span className=''>
+                        {`Aktive Zeit: ${artist?.['First Appearance']} -  ${artist?.['Last Appearance']}`}
+                      </span>
+                    </div>
                   </SheetHeader>
-                  <div className='grid gap-4 py-4'></div>
-                  <SheetFooter>
+                  <SheetDescription className='px-6 flex flex-col gap-6 mb-6'>
+                    <div className='flex flex-row gap-1'>
+                      {artist['Instrument']?.map((instrument) => (
+                        <Badge key={instrument} variant='outline'>{instrument}</Badge>
+                      ))}
+                    </div>
+                    <div className=''>
+                      <div>{`Geboren am: ${artist?.['Date of Birth']}`}</div>
+                      <div>{`Gestorben am: ${artist?.['Date of Death']}`}</div>
+                    </div>
+                    <p className='text-black'>{artist['Description']}</p>
+                    <div className='text-black'>
+                      <h2 className='text-xl font-bold'>Publikationen / Alben</h2>
+                      <div>{artist?.['Publications / Albums']}</div>
+                    </div>
+                    <div className='text-black'>
+                      <h2 className='text-xl	font-bold'>Bands / Formationen</h2>
+                      <div>{artist?.['Bands / Formations']}</div>
+                    </div>
+                  </SheetDescription>
+                  <SheetFooter className='px-6'>
                     <SheetClose asChild>
-                      <Button type='submit'>Save changes</Button>
+                      <Button type='submit'>Close</Button>
                     </SheetClose>
                   </SheetFooter>
                 </SheetContent>
